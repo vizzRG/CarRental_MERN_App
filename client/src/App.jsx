7069e26ffbd5f4d06ab272019798d49491c1c0b3
Vishal Tyagi <vizzrg@gmail.com>
2025-07-05 14:49:53 +0530

    Craeted Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,12 +11,15 @@ import Dashboard from "./pages/owner/Dashboard"
 import AddCar from "./pages/owner/AddCar"
 import ManageCars from "./pages/owner/ManageCars"
 import ManageBookings from "./pages/owner/ManageBookings"
+import Login from './components/Login'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
   const isOwnerPath = useLocation().pathname.startsWith("/owner")
   return (
     <>
+      {showLogin && <Login setShowLogin={setShowLogin}/>}
+
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
 
       <Routes>
