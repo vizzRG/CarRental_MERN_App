234dd09a7fd682a658154817eead905b48febf72
Vishal Tyagi <vizzrg@gmail.com>
2025-07-07 13:36:25 +0530

    Completed

--- server/server.js
+++ server/server.js
@@ -6,32 +6,29 @@ import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 
-// Initialize Express App
+//Initialize Express App
 const app = express();
-
-// ✅ Use .env value for CORS origin
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-}));
-app.options("*", cors());
 
 // Connect Database
 await connectDB();
-
 // Middleware
+// app.use(cors())
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173", // replace with your frontend URL
+  credentials: true,
+}));
+
 app.use(express.json());
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("Server is Running");
-});
-app.use("/api/user", userRouter);
-app.use("/api/owner", ownerRouter);
+app.get("/", (req, res) => {res.send("Server is Running");});
+app.use("/api/user", userRouter)
+app.use("/api/owner",ownerRouter)
 app.use("/api/bookings", bookingRouter);
 
-// Start Server
+
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
